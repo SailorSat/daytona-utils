983d8470d8a81c69a322d38076a09ec909eb69c1
Ariane Fugmann <sailorsat@t-online.de>
2017-06-20 19:21:58 +0200

    daytonas - upload complete script
    oops - for some reason I forgot to upload the whole script

--- src/scripts/daytonas.lua
+++ src/scripts/daytonas.lua
@@ -1,8 +1,14 @@
 require("model2");	-- Import model2 machine globals
 
 function Init()
+	-- enable special inputs (only usefull if special type cabinet selected)
 	Patch_SpecialInputs();
+	
+	-- reroute lamp data
 	Patch_LampOutputs();
+	
+	-- reroute drive data
+	Patch_DriveOutputs();
 end
 
 function Patch_SpecialInputs()
@@ -51,6 +57,22 @@ function Patch_LampOutputs()
 	for offset = 0x00000000, 0x0003FFFF, 4 do
 		if Romset_ReadDWord(0, offset) == 0x01C0001E then
 			Romset_PatchDWord(0, offset, 0x00500824);
+			local opcode = offset - 1;
+			if Romset_ReadByte(0, opcode) == 0x80 then
+				Romset_PatchByte(0, opcode, 0x90)	-- replace LDOB with LD
+			end
+			if Romset_ReadByte(0, opcode) == 0x82 then
+				Romset_PatchByte(0, opcode, 0x92)	-- replace STOB with ST
+			end
+		end
+	end
+end
+
+function Patch_DriveOutputs()
+	-- reroute 0x01C00022 to 0x00500828
+	for offset = 0x00000000, 0x0003FFFF, 4 do
+		if Romset_ReadDWord(0, offset) == 0x01C00022 then
+			Romset_PatchDWord(0, offset, 0x00500828);
 			local opcode = offset - 1;
 			if Romset_ReadByte(0, opcode) == 0x80 then
 				Romset_PatchByte(0, opcode, 0x90)	-- replace LDOB with LD
